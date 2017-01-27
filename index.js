b8ac851b3f6d47a49d50a80c32e0c344e43f130b
Amit <ahetawal@salesforce.com>
2017-01-27 15:04:09 -0800

    nodejs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname+'/public/ahetawal-p.html');
+  response.sendFile(__dirname+'/html/ahetawal-p.html');
 });
 
 app.listen(app.get('port'), function() {
